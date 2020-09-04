3e33687e286ea7531082d76bf125af8b0c7a7120
dxnxxl <ddspencer@bsu.edu>
2020-09-04 17:45:32 -0400

    methods pass tests

--- romanconvertor.rb
+++ romanconvertor.rb
@@ -1,5 +1,36 @@
-def roman_map
-  {
+def fromRoman(romanNumber)
+  if romanNumber =~ /[?=MDCLXVI]/
+    raise TypeError
+  end
+  reversed_map= {
+      "M" => 1000,
+      "CM" => 900,
+      "D" => 500,
+      "CD" => 400,
+      "C" => 100,
+      "XC" => 90,
+      "L" => 50,
+      "XL" => 40,
+      "X" => 10,
+      "IX" => 9,
+      "V" => 5,
+      "IV" => 4,
+      "I" => 1
+  }
+  result = 0
+  (0..romanNumber.length - 1).each { |i|
+    if i > 0 and reversed_map[romanNumber[i]] > reversed_map[romanNumber[i - 1]]
+      result += reversed_map[romanNumber[i]] - 2 * reversed_map[romanNumber[i - 1]]
+    else
+      result += reversed_map[romanNumber[i]]
+    end
+  }
+  result
+end
+
+
+def toRoman(arabicNumber)
+  roman_map = {
       1000 => "M",
       900 => "CM",
       500 => "D",
@@ -14,28 +45,15 @@ def roman_map
       4 => "IV",
       1 => "I"
   }
-end
-
-def fromRoman(romanNumber)
-  result = 0
-  str = self
-  roman_map.values.each do |roman|
-    while str.start_with?(roman)
-      result += roman_map.invert[roman]
-      str = str.slice(roman.length, str.length)
-    end
+  if arabicNumber>3999 || arabicNumber <=0
+    raise RangeError
   end
-  result
-end
-
-def toRoman(arabicNumber)
   result = ""
-  number = self
-  roman_map.keys.each do |divisor|
-    quotient, modulus = number.divmod(divisor)
-    result << roman_map[divisor] * quotient
-    number = modulus
-    end
-  result
+  roman_map.each do |value, roman_char|
+    result << roman_char * (arabicNumber/value)
+    arabicNumber %= value
   end
+  result
+end
+
 
